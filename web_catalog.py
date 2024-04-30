4b4d7b542ffd6a8089d4b2283133a0776fce50bf
usrachap <64814146+usrachap@users.noreply.github.com>
2024-04-30 12:50:37 -0500

    Update web_catalog.py

--- web_catalog.py
+++ web_catalog.py
@@ -1,16 +1,36 @@
-# Import python packages
-import streamlit as st
-import requests
+import streamlit 
 import snowflake.connector
-#from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+import pandas
 
-# Write directly to the app
-st.title("Zena's Amazing Athleisure Catalog")
-st.write(
-    """Pick a Sweatsuit color or style:
-    """)
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
+# connect to snowflake 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
 
-cnx = st.connection("snowflake")
-session = snx.session()
-my_dataframe = session.table("ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website").select(col('color_or_style'))
+# run a snowflake query and put it all in a var called my_catalog
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall() 
+
+# put the dafta into a dataframe 
+df = pandas.DataFrame(my_catalog) 
+
+# temp write the dataframe to the page so I Can see what I am working with 
+# streamlit.write(df) 
+# put the first column into a list 
+color_list = df[0].values.tolist() 
+# print(color_list)
+# Let's put a pick list here so they can pick the color 
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list)) 
+# We'll build the image caption now, since we can 
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!' 
+
+# use the option selected to go back and get all the info from the database
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
+df2 = my_cur.fetchone() 
+
+streamlit.image( df2[0], 
+                width=400, 
+                caption= product_caption ) 
+streamlit.write('Price: ', df2[1]) 
+streamlit.write('Sizes Available: ',df2[2]) 
+streamlit.write(df2[3])
