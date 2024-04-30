02a05375714b80d275f2271e8a3f86c1ee92d439
usrachap <64814146+usrachap@users.noreply.github.com>
2024-04-30 12:38:35 -0500

    Update web_catalog.py

--- web_catalog.py
+++ web_catalog.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+import requests
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -9,5 +10,6 @@ st.write(
     """Pick a Sweatsuit color or style:
     """)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = snx.session()
 my_dataframe = session.table("ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website").select(col('color_or_style'))
